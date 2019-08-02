8f3c529e6fec5d3d833ea771301c21f2406698c2
Hozefa Bharmal <hozefa.bharmal@gmail.com>
2019-08-01 17:25:30 -0700

    Setup Main Component Initial

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import Submission from './components/Submission';
+
+//This Main component exists as a hub for connecting my various components together
+//Because this assignment only asked for one real component, I am leaving this as a placeholder for future expansion
+export class Main extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={}
+    }
+    render(){
+        return(
+            <div>Hello</div>
+        )
+    }
+}
