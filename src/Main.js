adc28d07275faf11f1e3a0df12cfbf301aec01e1
Hozefa Bharmal <hozefa.bharmal@gmail.com>
2019-08-04 17:00:18 -0700

    Setup React Component Structure and Connected Data

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Landing from './components/Landing'
 
+
 // import Submission from './components/Submission';
 
 //This Main component exists as a hub for connecting my various components together
---
+++ src/components/Information.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+export default function Information (props){
+    return(
+        <div className="information">
+            <p> School Name: {props.schoolNameProp} </p>
+            <p> School URL: {props.schoolUrlProp} </p>
+            <p> School City: {props.schoolCityProp} </p>
+            <p> School State: {props.schoolStateProp} </p>
+            <p> School Zip: {props.schoolZipProp} </p>
+
+
+
+
+        </div>
+    )
+}
+
+// Information.defaultProps = {guessProp: "Not guessed yet"};
