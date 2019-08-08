45139440df2a56808b243bbf37f1379524c0bec6
Hozefa Bharmal <hozefa.bharmal@gmail.com>
2019-08-07 23:46:24 -0700

    Deployment changes

--- src/components/Information.js
+++ src/components/Information.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Information (props){
     return(
         <div className="information no-print">
-            <h1> University of Wisconsin-Madison</h1>
+            <h1 class="school-name"> University of Wisconsin-Madison</h1>
             <div className="flex-information-container">
                 {/* <div> <h2>School Name:</h2> <p>{props.schoolNameProp}</p> </div> */}
                 <div className="flex-vertical"> <h2>Website</h2> <p>{props.schoolUrlProp}</p> </div>
